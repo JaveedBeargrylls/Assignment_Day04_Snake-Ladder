806d91db3613db2204867c75474e81190eb16d4b
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-05-15 19:47:40 +0530

    [Javeed] modified to printing random variables works as dice

--- Snake_ladder.java
+++ Snake_ladder.java
@@ -3,8 +3,9 @@ public static void main(String[] args){
 System.out.println("Welcome to the simple Snake & Ladder Game");
 	// Starting Position of Player
 int player_position = 0;
-	// Printing the position of player
-System.out.println(" Starting position of player : "+player_position);
+	// printing the random Variable(1-6) as dies 
+int random = (int)(Math.random()*6) + 1;
+System.out.println(" Random variables from 1 - 6 : "+random);
 }
 }
 
