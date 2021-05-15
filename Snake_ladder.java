771785fbc5877619641a52ab39e9298800a6f5f2
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-05-15 19:21:30 +0530

    [Javeed] modified to the starting position of Player

--- Snake_ladder.java
+++ Snake_ladder.java
@@ -1,6 +1,10 @@
 public class Snake_ladder{
 public static void main(String[] args){
 System.out.println("Welcome to the simple Snake & Ladder Game");
+	// Starting Position of Player
+int player_position = 0;
+	// Printing the position of player
+System.out.println(" Starting position of player : "+player_position);
 }
 }
 
