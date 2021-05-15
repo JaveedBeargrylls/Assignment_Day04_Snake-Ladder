a37967cf0da0ecdef23a7bd44f15b08b7eaf99aa
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-05-15 20:37:11 +0530

    [Javeed] modified to Ladder and snake

--- Snake_ladder.java
+++ Snake_ladder.java
@@ -5,7 +5,21 @@ System.out.println("Welcome to the simple Snake & Ladder Game");
 int player_position = 0;
 	// printing the random Variable(1-6) as dies 
 int random = (int)(Math.random()*6) + 1;
-System.out.println(" Random variables from 1 - 6 : "+random);
+	// Checking for random values to set Ladder and Snake
+int check = (int)(Math.random()*3);
+	// the function of ladder and snake	
+if ( check == 1 )
+	// Ladder
+	player_position += random;
+else if ( check == 2 )
+	// Snake
+	player_position -= random;
+else
+	// remains same position 
+	player_position = player_position;
+
+System.out.println(" Position of Player : " +player_position);
+//System.out.println(" Random variables from 1 - 6 : "+random+"\t"+check);
 }
 }
 
